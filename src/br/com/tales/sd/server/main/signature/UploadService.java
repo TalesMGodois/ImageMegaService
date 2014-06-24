66c3a0f1a8ae36e0e283bbf4686931e6235219b7
TalesGodois <tales.godois@gmail.com>
2014-06-24 10:35:31 -0300

    Retirando dependencia de runnable

--- src/br/com/tales/sd/server/main/signature/UploadService.java
+++ src/br/com/tales/sd/server/main/signature/UploadService.java
@@ -2,18 +2,19 @@ package br.com.tales.sd.server.main.signature;
 
 import br.com.tales.sd.server.main.application.Upload;
 
+import java.nio.channels.AlreadyBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /**
  * Created by tales on 28/05/14.
  */
-public interface UploadService extends Remote,Runnable {
-    public String getName() throws RemoteException;
-
-    public String upload(String name) throws RemoteException;
+public interface UploadService extends Remote
 
 
 
+    public String getName() throws RemoteException,AlreadyBoundException;
+
+    public String upload(String name) throws RemoteException,AlreadyBoundException;
 
 }
